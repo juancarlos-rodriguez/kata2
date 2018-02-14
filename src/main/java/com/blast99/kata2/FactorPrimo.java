2acd4f993037355a1799c3b6d57e03763bc35ed2
Juan Carlos Rodriguez <j.rodriguez.arrieta@accenture.com>
2018-02-14 03:10:09 -0600

    REFACTOR: se mejora la condicional para evaluar cualquier numero menor a 1

--- src/main/java/com/blast99/kata2/FactorPrimo.java
+++ src/main/java/com/blast99/kata2/FactorPrimo.java
@@ -3,8 +3,7 @@ package com.blast99.kata2;
 public class FactorPrimo {
 
     public boolean esPrimo(int number) {
-        if (number == 1) return false;
-        if (number == 0) return false;
+        if (number <= 1) return false;
         for (int i = 2; i < number; i++) {
             if (number % i == 0) {
                 return false;
