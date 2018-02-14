9ce242e9f28b8fb12a954fcb9cfcd9ce7d80869a
Juan Carlos Rodriguez <j.rodriguez.arrieta@accenture.com>
2018-02-14 00:28:55 -0600

    Initial commit | base project

---
+++ src/main/java/com/blast99/kata2/Kata2Application.java
@@ -0,0 +1,12 @@
+package com.blast99.kata2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kata2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kata2Application.class, args);
+	}
+}
