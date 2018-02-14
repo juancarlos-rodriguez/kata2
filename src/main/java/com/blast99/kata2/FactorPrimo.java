e697161c6c4e6b2b0a4d28f9342303ed8c73d16c
Juan Carlos Rodriguez <j.rodriguez.arrieta@accenture.com>
2018-02-14 03:09:00 -0600

    GREEN: agrega condicional si el numero a evaluar es 0

--- src/main/java/com/blast99/kata2/FactorPrimo.java
+++ src/main/java/com/blast99/kata2/FactorPrimo.java
@@ -4,6 +4,7 @@ public class FactorPrimo {
 
     public boolean esPrimo(int number) {
         if (number == 1) return false;
+        if (number == 0) return false;
         for (int i = 2; i < number; i++) {
             if (number % i == 0) {
                 return false;
