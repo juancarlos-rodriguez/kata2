97842b513bc1cfdd07419bbc1167000473bc689f
Juan Carlos Rodriguez <j.rodriguez.arrieta@accenture.com>
2018-02-14 02:51:56 -0600

    GREEN: modifica la lógica de la función esPrimo para que pase la prueba

--- src/main/java/com/blast99/kata2/FactorPrimo.java
+++ src/main/java/com/blast99/kata2/FactorPrimo.java
@@ -3,7 +3,7 @@ package com.blast99.kata2;
 public class FactorPrimo {
 
     public boolean esPrimo(int number) {
-        for (int i = 1; i < number; i++) {
+        for (int i = 2; i < number; i++) {
             if (number % i == 0) {
                 return false;
             }
